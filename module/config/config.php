0b7999933af3e85490cec2d18d4319181af5d0b0
Thomas Schabacher <schabacher@dmkzwo.de>
2017-05-12 16:57:03 +0200

    asset integration links fixed

--- module/config/config.php
+++ module/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
 if (TL_MODE == 'BE') {
-  $GLOBALS['TL_CSS'][] = 'system/modules/dz_backend/assets/pageshortcuts.css|screen';
-  $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/dz_backend/assets/jquery.min.js';
-  $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/dz_backend/assets/pageshortcuts.js';
+  $GLOBALS['TL_CSS'][] = 'system/modules/pageshortcuts/assets/pageshortcuts.css|screen';
+  $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/pageshortcuts/assets/jquery.min.js';
+  $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/pageshortcuts/assets/pageshortcuts.js';
 }
 
 $GLOBALS['pageshortcuts']['allowed_users'] = ['admin'];
